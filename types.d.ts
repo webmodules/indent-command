393cdf11050170814ff0dfe725f2dec981ef09df
Nathan Rajlich <nathan@tootallnate.net>
2015-01-29 16:03:33 -0800

    types: even more accurate "dom-iterator" type definition

--- types.d.ts
+++ types.d.ts
@@ -26,28 +26,55 @@ declare module "debug" {
 declare module "dom-iterator" {
   class Iterator {
     constructor(node: Node, root?: Node);
-    public reset(node?: Node): Iterator;
-    public revisit(revisit?: boolean): Iterator;
-    public opening(): Iterator;
-    public atOpening(): Iterator;
-    public closing(): Iterator;
-    public atClosing(): Iterator;
-    public next(type?: number, n?: number): Node;
-    public prev(type?: number, n?: number): Node;
-    public previous(type?: number, n?: number): Node;
-    public select(expr: number): Iterator;
-    public select(expr: string): Iterator;
-    public select(expr: (node: Node) => boolean): Iterator;
-    public selects(node: Node, peek: boolean): boolean;
-    public reject(expr: number): Iterator;
-    public reject(expr: string): Iterator;
-    public reject(expr: (node: Node) => boolean): Iterator;
-    public rejects(node: Node, peek: boolean): boolean;
-    public higher(node: Node): boolean;
-    public compile(expr: any): (node?: Node) => boolean;
-    public peak(expr?: any, n?: number): Node;
-    public peek(expr?: any, n?: number): Node;
-    public use(fn: (iterator: Iterator) => void ): Iterator;
+
+    reset(node?: Node): Iterator;
+    revisit(revisit?: boolean): Iterator;
+
+    opening(): Iterator;
+    atOpening(): boolean;
+
+    closing(): Iterator;
+    atClosing(): boolean;
+
+    next(): Node;
+    next(expr: number): Node;
+    next(expr: string): Node;
+    next(expr: (node: Node, peek?: boolean) => boolean): Node;
+
+    prev(): Node;
+    prev(expr: number): Node;
+    prev(expr: string): Node;
+    prev(expr: (node: Node, peek?: boolean) => boolean): Node;
+
+    previous(): Node;
+    previous(expr: number): Node;
+    previous(expr: string): Node;
+    previous(expr: (node: Node, peek?: boolean) => boolean): Node;
+
+    select(expr: number): Iterator;
+    select(expr: string): Iterator;
+    select(expr: (node: Node, peek?: boolean) => boolean): Iterator;
+
+    selects(node: Node, peek?: boolean): boolean;
+
+    reject(expr: number): Iterator;
+    reject(expr: string): Iterator;
+    reject(expr: (node: Node, peek?: boolean) => boolean): Iterator;
+
+    rejects(node: Node, peek?: boolean): boolean;
+
+    higher(node: Node): boolean;
+
+    compile(expr: number): (node: Node, peek?: boolean) => boolean;
+    compile(expr: string): (node: Node, peek?: boolean) => boolean;
+    compile(expr: (node: Node, peek?: boolean) => boolean): (node: Node, peek?: boolean) => boolean;
+
+    peek(): Node;
+    peek(expr: number, n?: number): Node;
+    peek(expr: string, n?: number): Node;
+    peek(expr: (node: Node, peek?: boolean) => boolean, n?: number): Node;
+
+    use(fn: (iterator: Iterator) => void ): Iterator;
   }
   export = Iterator;
 }
